864ddef01708feca59c9e015e00c222a91b99517
saelani <saelani@gmail.com>
2020-06-28 22:27:50 +0700

    berhasil menampilkan data kategori

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require("./koneksi");
 exports.index = function (req, res) {
   response.ok("Aplikasi REST API ku berjalan", res);
 };
+
+
+//menampilkan semua data kategori
+exports.getsKategori = function(req,res){
+    connection.query('SELECT * FROM kategori', function(error,rows,fileds){
+        if(error){
+            console.log(error);
+        } else {
+            response.ok(rows,res)
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -4,4 +4,6 @@ module.exports = function (app) {
   var jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
+
+  app.route('/kategori').get(jsonku.getsKategori)
 };
