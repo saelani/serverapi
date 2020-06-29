ec804e06dd015e38c4cf0f872f744b307a85f202
saelani <saelani@gmail.com>
2020-06-29 23:04:08 +0700

    login berhasil

--- middleware/index.js
+++ middleware/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
-var aut = require('./auth');
+var auth = require('./auth');
 var router = express.Router();
 
-router.post('/api/vi/register', aut.registrasi);
+router.post('/api/vi/register', auth.registrasi);
+router.post('/api/vi/login', auth.login);
 
 module.exports = router;
